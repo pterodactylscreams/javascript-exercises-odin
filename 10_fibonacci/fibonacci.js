977c39e665c872c3f1bde031c99b405e89f5e6cb
pterodactylscreams <pterodactylscreams@gmail.com>
2025-02-11 15:14:17 +0530

    fibonacci 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(numArg) {
+    // 1,1,2,3,5,8
 
+    if (typeof(numArg)!=Number){
+        num = parseInt(numArg)
+    }
+    else {
+        num = numArg
+    }
+
+    
+    if (num <0 ){
+        return "OOPS"
+    }
+    if (num ==0 ){
+        return 0
+    }
+
+    if (num==1 || num ==2){
+        return 1
+    }
+
+    fib1=0
+    fib2=1
+
+    for (let i = 2; i <= num; i++){
+        res=fib1+fib2
+        fib1=fib2;
+        fib2=res
+    }
+
+    return res
 };
 
 // Do not edit below this line
