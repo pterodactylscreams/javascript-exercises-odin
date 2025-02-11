8c93219e95ce185206adf928de062f5ae392e645
pterodactylscreams <pterodactylscreams@gmail.com>
2025-02-11 16:02:27 +0530

    findOldest 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currYear=new Date().getFullYear()
+    const agesTable= people.map(person => ( 
+        {
+        ...person, 
+        age: (!person.yearOfDeath ? currYear : person.yearOfDeath) - person.yearOfBirth 
+        }
+    )
+    );
 
-};
+
+const sortAge=agesTable.sort((p1,p2) => p2.age - p1.age)
+
+return sortAge[0]
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
