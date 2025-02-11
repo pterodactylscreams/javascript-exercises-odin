3ad213b97f74cb0166918fa9a997f157654662b3
pterodactylscreams <pterodactylscreams@gmail.com>
2025-02-11 14:23:57 +0530

    palindromes 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (st1) {
+    const allowed= "abcdefghijklmnopqrstuvwxyz1234567890"
+    
+    const cleanst= st1
+                    .toLowerCase()
+                    .split("")
+                    .filter((chara) => allowed.includes(chara))
+                    .join("")
 
+                    // return cleanst
+
+                    const reversest=cleanst.split("").reverse().join("")
+
+                    return reversest==cleanst
 };
 
 // Do not edit below this line
